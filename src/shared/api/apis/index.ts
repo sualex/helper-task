e905b05af78a1cf70e3686310493a2a658bbac34
sualexx <a153048@gmail.com>
2024-01-28 16:33:59 +0300

    generate api

---
+++ src/shared/api/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from "./AuthApi";
+export * from "./ImageApi";
+export * from "./ProfileApi";
+export * from "./UserApi";
---
+++ src/shared/api/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from "./ApiKeyDto";
+export * from "./ImageDto";
+export * from "./LoginDto";
+export * from "./ProfileDto";
+export * from "./SignUpDto";
+export * from "./UpdateProfileDto";
