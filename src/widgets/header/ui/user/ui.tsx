a123f65f710f9c059dff9c93582e4aa84602b078
sualexx <a153048@gmail.com>
2024-01-31 22:01:23 +0300

    tune header user

--- src/widgets/header/ui/user/ui.tsx
+++ src/widgets/header/ui/user/ui.tsx
@@ -9,13 +9,6 @@ import { LoginButton } from "@/entities/user/ui/login";
 export const HeaderUser = ({ ...props }: StackProps) => {
   const theme = useTheme();
   const myProfile = useMyProfile();
-
-  if (myProfile?.isValidating) {
-    return <UserAvatar />;
-  }
-
-  console.log("xxxxxxxxxxxxxxxxxxxx ", myProfile?.data);
-
   return myProfile?.data ? (
     <Button
       href={`/profile/${myProfile?.data?.slug}`}
