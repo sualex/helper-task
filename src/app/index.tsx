ac4d3a493bb261c2ccdde756882a502f5b148cfe
sualexx <a153048@gmail.com>
2024-02-07 20:28:05 +0300

    remove slug adornment on mobile

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -17,7 +17,8 @@ const App = (props: AppProps) => {
   return (
     <>
       <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <title>Yoldi</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <SWRDevTools>
         <MuiProvider {...props}>
--- src/entities/user/ui/slug/ui.tsx
+++ src/entities/user/ui/slug/ui.tsx
@@ -5,36 +5,42 @@ import { useRouter } from "next/router";
 import * as React from "react";
 import { TextFieldElement, TextFieldElementProps } from "react-hook-form-mui";
 
-import { useCommon } from "@/shared/lib";
+import { useCommon, useMediaDown } from "@/shared/lib";
 
 export function Slug({ ...props }: Omit<TextFieldElementProps, "variant">) {
   const router = useRouter();
   const { theme } = useCommon();
+
+  const isMobile = useMediaDown("sm");
+
   return (
     <TextFieldElement
       InputProps={{
-        startAdornment: (
-          <InputAdornment
-            position="start"
-            css={css`
-              &&& {
-                margin-right: 0;
-                margin-left: -8px;
-                padding: 0 20px;
-                max-height: revert;
-                height: 50px;
-                background: ${theme?.palette?.backgroundSecondary?.main};
-                border-top-left-radius: 5px;
-                border-bottom-left-radius: 5px;
-                border-right: 1px solid ${theme?.palette?.strokesPrimary?.main};
-              }
-            `}
-          >
-            <NoSsr>
-              <Typography>{`${window.location.host}/${router?.route?.split("/")?.[1]}`}</Typography>
-            </NoSsr>
-          </InputAdornment>
-        ),
+        ...(!isMobile && {
+          startAdornment: (
+            <InputAdornment
+              position="start"
+              css={css`
+                &&& {
+                  margin-right: 0;
+                  margin-left: -8px;
+                  padding: 0 20px;
+                  max-height: revert;
+                  height: 50px;
+                  background: ${theme?.palette?.backgroundSecondary?.main};
+                  border-top-left-radius: 5px;
+                  border-bottom-left-radius: 5px;
+                  border-right: 1px solid
+                    ${theme?.palette?.strokesPrimary?.main};
+                }
+              `}
+            >
+              <NoSsr>
+                <Typography>{`${window.location.host}/${router?.route?.split("/")?.[1]}`}</Typography>
+              </NoSsr>
+            </InputAdornment>
+          ),
+        }),
       }}
       css={css`
         &&& {
