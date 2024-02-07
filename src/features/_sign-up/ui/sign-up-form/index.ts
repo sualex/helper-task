7a82f25f3121a7f39392e628babf358a7ea73f62
sualexx <a153048@gmail.com>
2024-02-07 14:04:12 +0300

    clean up

--- src/features/_sign-up/ui/sign-up-form/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./ui";
-export { signUpFormValidationSchema } from "./model";
--- src/features/_sign-up/ui/sign-up-form/ui/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "../../../../sign-up/ui/sign-up-form/ui/name";
-export * from "./submit-button";
