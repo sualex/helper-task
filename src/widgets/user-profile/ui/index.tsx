2e9f83a99dcdf992608d1874e263fd5c1ad49ee1
sualexx <a153048@gmail.com>
2024-02-12 16:24:36 +0300

    fix edit profile link by design

--- src/widgets/user-profile/ui/index.tsx
+++ src/widgets/user-profile/ui/index.tsx
@@ -6,12 +6,13 @@ import { useUser } from "@/entities/user/model";
 import { UserAvatar } from "@/entities/user/ui/avatar";
 import { EditProfileLink } from "@/features/edit-profile";
 import { LogoutLink } from "@/features/logout";
-import { useCommon } from "@/shared/lib";
+import { useCommon, useMediaDown } from "@/shared/lib";
 import { Article, H1 } from "@/shared/ui";
 import { useIsMyProfile } from "@/widgets/user-profile/model";
 
 export const UserProfile = ({ ...props }: StackProps) => {
   const { pxToRem, theme } = useCommon();
+  const sm = useMediaDown("md");
   const profile = useUser();
   const isMyProfile = useIsMyProfile();
   return (
@@ -28,7 +29,7 @@ export const UserProfile = ({ ...props }: StackProps) => {
       <Stack spacing={pxToRem(60)}>
         <Stack>
           <Grid container rowSpacing="8px">
-            <Grid xs={12} md={8}>
+            <Grid xs={12} md={9}>
               <Stack spacing={1}>
                 <H1 lineHeight="42px">
                   {profile ? profile?.data?.name : <Skeleton />}
@@ -47,12 +48,16 @@ export const UserProfile = ({ ...props }: StackProps) => {
               </Stack>
             </Grid>
             {isMyProfile && (
-              <Grid xs={12} md={4}>
-                <EditProfileLink />
+              <Grid xs={12} md={3}>
+                <EditProfileLink
+                  css={css`
+                    width: ${sm ? "auto" : "100%"};
+                  `}
+                />
               </Grid>
             )}
             {profile?.data?.description && (
-              <Grid xs={12} md={8}>
+              <Grid xs={12} md={9}>
                 <Typography
                   css={css`
                     font-size: 16px;
